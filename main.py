88bc5452170e967177ac27616d0d06e4e08e0293
tmwatchanan <tmwatchanan@gmail.com>
2018-02-26 10:40:59 +0700

    Change from using direct deepcut to tokenize from pythainlp

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import deepcut
-list_word = deepcut.tokenize('ตัดคำได้ดีมาก')
+from pythainlp.tokenize import word_tokenize
+list_word = word_tokenize(text='ผมรักคุณนะครับโอเคบ่พวกเราเป็นคนไทยรักภาษาไทยภาษาบ้านเกิด',engine='deepcut')
 with open('output.txt', 'w', newline='', encoding='utf-8') as file:
     file.write('|'.join(list_word))
